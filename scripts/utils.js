22be770f1e260ab61a6c55dae74deded2ffb22d7
Lyove <lyove@outlook.com>
2022-07-19 15:30:29 +0800

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -9,36 +9,40 @@ const devOutputName = "editor";
 const bundleSuffix = "bundle";
 const port = 8000;
 
-/**
- * Boxen options as defined by https://www.npmjs.com/package/boxen
- * @type {{padding: {top: number, left: number, bottom: number, right: number}}}
- */
-const boxenOptions = {
-  padding: {
-    left: 13,
-    right: 13,
-    top: 1,
-    bottom: 1,
-  },
-};
-
-/**
- * Figlet options as defined by https://github.com/patorjk/figlet.js
- * @type {{horizontalLayout: string, font: string}}
- */
-const figletOptions = {
-  font: "Small Slant",
-  horizontalLayout: "Standard",
-};
 
 /**
  * Package info
  */
 const { name, version, author, license } = packageJson;
-const details = chalk.cyan(`${author} | Version ${version} | License ${license} `);
+const boxDetails = chalk.cyan(`${author} | Version ${version} | License ${license} `);
 function packInfo() {
-  console.log(chalk.cyan(figlet.textSync(name, figletOptions)));
-  console.log(boxen(details, boxenOptions), "\n");
+  console.log(
+    chalk.cyan(
+      /**
+       * Figlet options as defined by https://github.com/patorjk/figlet.js
+       * @type {{horizontalLayout: string, font: string}}
+       */
+      figlet.textSync(name, {
+        font: "Small Slant",
+        horizontalLayout: "Standard",
+      })
+    )
+  );
+  console.log(
+    /**
+     * Boxen options as defined by https://www.npmjs.com/package/boxen
+     * @type {{padding: {top: number, left: number, bottom: number, right: number}}}
+     */
+    boxen(boxDetails, {
+      padding: {
+        left: 13,
+        right: 13,
+        top: 1,
+        bottom: 1,
+      },
+    }),
+    "\n"
+  );
 }
 
 /**
